b5d536c32501d0a73d464592ad9f36863c1eb9e1
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-02-19 00:17:08 +0300

    added tests app

---
+++ src/components/loader/loader.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import Loader from './loader';
+
+describe("Test for Loader component", () => {
+  test('renders Loader component correctly', () => {
+    render(<Loader />);
+    const textElement = screen.getByText('Загружаем информацию...');
+    // Проверяем, что компонент успешно отрендерился
+    expect(textElement).toBeInTheDocument();
+  });
+});
--- src/components/search/Search.test.jsx
+++ src/components/search/Search.test.jsx
@@ -1,20 +1,16 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import Search from './Search';
+import { render, screen, fireEvent } from '@testing-library/react'
+import Search from './Search'
 
-describe("Test for Input", () => {
+describe('Test for Input', () => {
+  test('renders placeholder and sets userLogin correctly', () => {
+    const setUserLoginMock = jest.fn() // Создаем mock-функцию для setUserLogin
+    render(<Search setUserLogin={setUserLoginMock} />)
+    const inputEl = screen.getByPlaceholderText('Введите ваш запрос...')
+    fireEvent.change(inputEl, { target: { value: 'testValue' } })
 
-test('renders placeholder', () => {
-  render(<Search />);
-  const inputEl = screen.getByPlaceholderText("Введите ваш запрос...");
-  expect(inputEl).toBeInTheDocument();
-});
-
-test('sets userLogin correctly', () => {
-    const setUserLoginMock = jest.fn(); // Создаем mock-функцию для setUserLogin
-    render(<Search setUserLogin={setUserLoginMock} />);
-    const inputEl = screen.getByPlaceholderText("Введите ваш запрос...");
-    fireEvent.change(inputEl, { target: { value: 'testValue' } });
-    expect(setUserLoginMock).toHaveBeenCalledWith('testValue');
-  });
-
+    //Проверяем верный рендер компонента
+    expect(inputEl).toBeInTheDocument()
+    //Проверяем, что верно устанавливается userLogin
+    expect(setUserLoginMock).toHaveBeenCalledWith('testValue')
+  })
 })
