9adfba026889af3e7b8d1f44f3ae3b1364eedfd9
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-02-19 11:10:04 +0300

    fix sort comp

--- src/components/sortComp/SortComponent.jsx
+++ src/components/sortComp/SortComponent.jsx
@@ -1,19 +1,14 @@
 import * as S from './styles'
-import { useEffect, useState } from 'react'
 
 const SortComponent = ({ setOrder }) => {
-  const [sort, setSort] = useState(true)
-
-  useEffect(() => {
-    sort ? setOrder('desc') : setOrder('asc')
-  }, [sort])
 
   return (
     <S.Container>
-      <S.Button onClick={() => setSort(!sort)}>
-        {sort
-          ? 'Фильтровать по убыванию числа репозиториев'
-          : 'Фильтровать по возрастанию числа репозиториев'}
+      <S.Button onClick={() => setOrder('asc')}>
+        Фильтровать по возрастанию числа репозиториев
+      </S.Button>
+      <S.Button onClick={() => setOrder('desc')}>
+        Фильтровать по убыванию числа репозиториев
       </S.Button>
     </S.Container>
   )
