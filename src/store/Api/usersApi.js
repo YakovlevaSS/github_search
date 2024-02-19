c7615957e5641bedc89a33661635ba7c2db5d752
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-02-19 23:21:40 +0300

    del token

--- src/store/Api/usersApi.js
+++ src/store/Api/usersApi.js
@@ -1,14 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-const token = process.env.REACT_APP_API_TOKEN
 
 export const usersApi = createApi({
   reducerPath: 'usersApi',
   tagTypes: ['USERS'],
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://api.github.com/',
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
   }),
   endpoints: (builder) => ({
     getUsers: builder.query({
